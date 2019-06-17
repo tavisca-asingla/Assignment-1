a5f66147174cd36a4f38abac5229299e23257d97
Aniket <asingla@tavisca.in>
2019-06-17 23:05:40 +0530

    Hello uses Greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,7 @@
+require 'greeter'
+
 # Default is World
-skdfsjdlkfjkl (1singlaaniket@gmail.com)
 name = ARGV.first || "World"
 
-puts "Hello, #{name}!"
+greeter = Greeter.new(name)
+puts greeter.greet
